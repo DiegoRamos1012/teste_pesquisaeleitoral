19aaedb16fe8cb8ce500d719e9fcfee83889c698
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-03-15 14:33:11 -0300

    Initial Commit

---
+++ pesquisaeleitoral/src/main/java/com/diegoramos/konatus/pesquisaeleitoral/PesquisaeleitoralApplication.java
@@ -0,0 +1,13 @@
+package com.diegoramos.konatus.pesquisaeleitoral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaeleitoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaeleitoralApplication.class, args);
+	}
+
+}
---
+++ pesquisaeleitoral/src/test/java/com/diegoramos/konatus/pesquisaeleitoral/PesquisaeleitoralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoramos.konatus.pesquisaeleitoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaeleitoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
